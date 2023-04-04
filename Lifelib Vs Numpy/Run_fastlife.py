29c161f32a9608fab1eac1043808a1b7866787e6
robin <robin.wengi@outlook.com>
2023-04-05 00:44:11 +0200

    pv_net working with Numpy

--- Lifelib Vs Numpy/Run_fastlife.py
+++ Lifelib Vs Numpy/Run_fastlife.py
@@ -11,13 +11,16 @@ pols = proj.Policy.PolicyData()
 pv_prem = proj.PV_PremIncome(0)
 pv_claim = proj.PV_BenefitTotal(0)
 pv_net = proj.PV_NetCashflow(0)
+premInc = proj.PremIncome(0)
+benefitTotal = proj.BenefitTotal(0)
 
 for t in range(1,pols['PolicyTerm'].max()+1):
 
     pv_prem = pd.concat([pv_prem,proj.PV_PremIncome(t)],axis=1)
     pv_claim = pd.concat([pv_claim, proj.PV_BenefitTotal(t)], axis=1)
     pv_net = pd.concat([pv_net, proj.PV_NetCashflow(t)], axis=1)
-
+    premInc = pd.concat([premInc,proj.PremIncome(t)],axis=1)
+    benefitTotal = pd.concat([benefitTotal,proj.BenefitTotal(t)],axis=1)
 
 NetCashflow = pv_prem.to_numpy() + pv_claim.to_numpy()
 
--- Lifelib Vs Numpy/Run_numpy.py
+++ Lifelib Vs Numpy/Run_numpy.py
@@ -2,7 +2,6 @@ import pandas as pd
 import time
 import numpy as np
 import os
-
 path = os.path.abspath(os.getcwd())
 
 start_time = time.time()
@@ -13,72 +12,53 @@ mt = pd.read_excel(path +'/fastlife/model/Input/MortalityTables.xlsx', header=1)
 
 pol = pd.read_excel(path +'/fastlife/model/Input/PoliyData.xlsx')
 
-
-
-n_max = pol['MaxPolicyTerm'].max()
+n_max = pol['PolicyTerm'].max() + 1
 number_policy = len(pol)
 
 # Create a zero and one numpy array of n_max and number_policy length
 zeros = np.zeros((number_policy, n_max))
 ones = np.ones((number_policy, n_max))
 
-
-lapse = ones * 0.1
+# Create age vector
+age = ones.copy().cumsum(axis=1) + pol['IssueAge'].to_numpy()[:, np.newaxis] - 1
 
 # Do the vectorized Qx
 mt = mt.rename(columns={"Unnamed: 0": "age"}).set_index('age').T
 qx_table = pol[['Sex']].merge(mt,how='left',left_on='Sex',right_index=True).to_numpy()[:,1:]
-
-
-# Create age vector
-age_end = ones * pol['MaxPolicyTerm'].to_numpy()[:, np.newaxis]
-age = (ones).cumsum(axis=1) + pol['IssueAge'].to_numpy()[:, np.newaxis] - 1
-
-# we max age at 130 (max mt)
-age[age>=130] = (ones * 130)[age>=130]
-
-# ccreate vector
 qx = np.take_along_axis(qx_table,age.astype(int),axis=1)
 
 
-# todo à vérifier
-duration = ones.cumsum(axis=1)
-
-# Lapse rate
-# lapse_rate = np.maximum(0.1 - 0.02 * duration, ones * 0.02)
-#
-# pols_maturity = zeros.copy()
-#
-# pols_death = zeros.copy()
-# pols_if = zeros.copy()
-# pols_lapse = zeros.copy()
-policy_term = ones * (pol['MaxPolicyTerm']).to_numpy()[:, np.newaxis]
-#
-# pols_if[:, 0] = 1 # Same as pol_init
-
-
+duration = ones.copy().cumsum(axis=1) -1
+policy_term = ones.copy() * (pol['PolicyTerm']).to_numpy()[:, np.newaxis]
 PolsMaturity = zeros.copy()
 PolsIF_End = zeros.copy()
-PolsIF_AftMat =zeros.copy()
-PolsIF_End = zeros.copy()
-PolsMaturity = zeros.copy()
 PolsDeath = zeros.copy()
+PolsIF_AftMat = zeros.copy()
 
+PolsIF_AftMat[:,0]=1
+PolsDeath[:,0] = qx[:, 0]
 
 # Inforces
-for i in range(0, n_max):
-    PolsIF_End[:, i] = PolsIF_AftMat[:, i - 1] - PolsDeath[:, i - 1]
-    PolsMaturity[:, i] = (policy_term[:, i] == duration[:, i]) * PolsIF_End[:, i]
-
-    if i == 0:
-        PolsIF_AftMat[:, i] = ones.copy()[:, i]
-    else:
-        PolsIF_AftMat[:, i] = PolsIF_End[:, i] - PolsMaturity[:, i]
-
-    PolsDeath[:, i] = PolsIF_AftMat[:, i] * qx[:, i] # * asmp.MortFactor(t)
+for t in range(1, n_max):
+    PolsIF_End[:, t] = PolsIF_AftMat[:, t - 1] - PolsDeath[:, t - 1]
+    PolsMaturity[:, t] = (policy_term[:, t] == duration[:, t]) * PolsIF_End[:, t]
+    PolsIF_AftMat[:, t] = PolsIF_End[:, t] - PolsMaturity[:, t]
+    PolsDeath[:, t] = PolsIF_AftMat[:, t] * qx[:, t]
 
 
+PremInc = ones.copy() * pol['Premium'].to_numpy()[:, np.newaxis] * PolsIF_AftMat
+BenefitDeath = ones.copy() * pol['SumAssured'].to_numpy()[:, np.newaxis] * PolsDeath * (-1)
 
+pv_prem = zeros.copy()
+pv_claim = zeros.copy()
+
+
+for t in range(0,n_max):
+    discount_factor = 1 / (1+i)**(ones.copy()[:,t:].cumsum(axis=1)-1)
+    pv_prem[:, t] = (PremInc[:,t:] * discount_factor).sum(axis=1)
+    pv_claim[:, t] = (BenefitDeath[:, t:] * discount_factor/(1+i)).sum(axis=1)
+
+pv_net = pv_prem + pv_claim
 
 
 print("numpy approach run in %.2f seconds with 10K policies" % (time.time() - start_time))
